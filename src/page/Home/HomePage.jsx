fd0ac743b8badaa6168fc75c0a769e6eca4ddd4f
Nguyen Van Hoang <Hoangculus2.5@gmail.com>
2025-10-05 16:27:33 +0700

    Feat: Update json NewArrival; Add Review

--- src/page/Home/HomePage.jsx
+++ src/page/Home/HomePage.jsx
@@ -1,9 +1,11 @@
 import NewArrivals from "./NewArrivals/Page";
+import Review from "./Review/Page";
 export default function HomePage() {
     return (
         <div>
             <h1>Home Page code ở đâyyyyyyyyyy</h1>
             <NewArrivals />
+            <Review />
         </div>
     )
 }
