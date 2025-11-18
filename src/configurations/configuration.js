7d8979383280dac464de7223c5cd3b6c8a8d3c6a
Nguyen Van Hoang <Hoangculus2.5@gmail.com>
2025-11-18 23:18:53 +0700

    Add redirect URI for production environment in OAuth configuration

--- src/configurations/configuration.js
+++ src/configurations/configuration.js
@@ -3,6 +3,7 @@ export const OAuthConfig = {
         clientId: "890784479135-ooghqs178bf7b9orpvkac4jlt9l1tn4a.apps.googleusercontent.com",
         redirectUri: [
             "http://localhost:5173/auth/OAuth",
+            "https://lok-fe.vercel.app/auth/OAuth",
             "https://bephevch.up.railway.app/auth/OAuth",
             "https://befinal.up.railway.app/auth/OAuth"
         ],
