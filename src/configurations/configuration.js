7b2e3c250374df2f0f51b6b3e7acc02fab2202da
Nguyen Van Hoang <Hoangculus2.5@gmail.com>
2025-11-18 16:45:20 +0700

    Update O2

--- src/configurations/configuration.js
+++ src/configurations/configuration.js
@@ -1,7 +1,11 @@
 export const OAuthConfig = {
     Google: {
         clientId: "890784479135-ooghqs178bf7b9orpvkac4jlt9l1tn4a.apps.googleusercontent.com",
-        redirectUri: ["http://localhost:5173/auth/OAuth", "https://bephevch.up.railway.app/auth/OAuth"],
+        redirectUri: [
+            "http://localhost:5173/auth/OAuth",
+            "https://bephevch.up.railway.app/auth/OAuth",
+            "https://befinal.up.railway.app/auth/OAuth"
+        ],
         authUri: "https://accounts.google.com/o/oauth2/auth",
     },
 }
--- src/service/Base.jsx
+++ src/service/Base.jsx
@@ -1,4 +1,8 @@
 export const BASE_URL = 'http://localhost:3000'
 // export const base = 'https://bephevl.up.railway.app' // Be cũ
-export const base = 'https://bephevch.up.railway.app'
-export const localhost = 'http://localhost:8080'
+// export const base = 'https://bephevch.up.railway.app'
+// export const localhost = 'http://localhost:8080'
+// export const localhost = 'https://bephevch.up.railway.app'
+
+export const base = 'https://befinal.up.railway.app'
+export const localhost = 'https://befinal.up.railway.app'
